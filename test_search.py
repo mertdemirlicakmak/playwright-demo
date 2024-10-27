de6db8a2e760fcf7f56960b4b788c1a04e7aa7cf
Can Mert Demirliçakmak <mert@Cans-Mac-mini.local>
2024-10-27 15:12:43 +0300

    refactor test

--- test_search.py
+++ test_search.py
@@ -24,50 +24,51 @@ def page_context():
         browser.close()
 
 
+@pytest.fixture
+def homepage(page_context):
+    home = KiwiHomePage(page_context)
+    home.open()
+    home.click_close_cookies_window()
+    return home
+
+
 @given("As an not logged user navigate to homepage https://www.kiwi.com/en/")
-def navigate_to_homepage(page_context):
-    homepage = KiwiHomePage(page_context)
-    homepage.open()
-    homepage.click_close_cookies_window()
+def navigate_to_homepage(homepage):
+    pass  # The homepage is already opened in the fixture
 
 
 @when("I select one-way trip type")
-def select_one_way_trip(page_context):
-    homepage = KiwiHomePage(page_context)
+def select_one_way_trip(homepage):
     homepage.select_one_way_trip()
 
 
 @when("Set as departure airport RTM")
-def set_departure_airport(page_context):
-    homepage = KiwiHomePage(page_context)
+def set_departure_airport(homepage):
     homepage.set_departure_airport("RTM")
 
 
 @when("Set the arrival Airport MAD")
-def set_arrival_airport(page_context):
-    homepage = KiwiHomePage(page_context)
+def set_arrival_airport(homepage):
     homepage.set_arrival_airport("MAD")
 
 
 @when("Set the departure time 1 week in the future starting current date")
-def set_departure_date(page_context):
+def set_departure_date(homepage):
     date_in_one_week = (datetime.now() + timedelta(days=7)).strftime("%Y-%m-%d")
-    homepage = KiwiHomePage(page_context)
     homepage.set_departure_date(date_in_one_week)
 
 
 @when("Uncheck the `Check accommodation with booking.com` option")
-def uncheck_booking_checkbox(page_context):
-    homepage = KiwiHomePage(page_context)
+def uncheck_booking_checkbox(homepage):
     homepage.uncheck_booking_checkbox()
 
 
 @when("Click the search button")
-def click_search_button(page_context):
-    homepage = KiwiHomePage(page_context)
+def click_search_button(homepage):
     homepage.click_search_button()
 
 
 @then("I am redirected to search results page")
-def verify_search_results(page_context):
-    assert "search/results" in page_context.url
+def verify_search_results(homepage):
+    current_url = homepage.page.url
+    assert "search/results" in current_url
